4a00f333287d30412c264eb73cf6ec7e7fadeb05
uimaxbai <61615730+uimaxbai@users.noreply.github.com>
2025-03-20 11:53:46 +0000

    Pending changes exported from your codespace

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -3,6 +3,6 @@ import type { Handle } from '@sveltejs/kit';
 export const handle: Handle = async ({ event, resolve }) => {
     // console.log(event.getClientAddress());
     event.locals.ip = event.getClientAddress();
-    console.log(event.locals.ip);
+    // console.log(event.locals.ip);
     return await resolve(event);
 }
--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -3,9 +3,14 @@ import { IPINFO_API_KEY } from "$env/static/private";
 export async function load({ locals }) {
     // console.log(locals.ip);
     try {
-        const clientIP: string = locals.ip;
-        const response = await fetch(`https://ipinfo.io/${clientIP}?token=${IPINFO_API_KEY}`);
-        const data = await response.json();
+        // console.log(IPINFO_API_KEY)
+        var clientIP: string = locals.ip;
+        // clientIP = "1.1.1.1"
+        // console.log(clientIP);
+        // console.log(1);
+        let response = await fetch(`https://ipinfo.io/${clientIP}/json?token=${IPINFO_API_KEY}`);
+        let data = await response.json();
+        console.log(data)
         const country = data.country;
         const city = data.city;
         const region = data.region;
